4e0c2ddf263cd77cc55c4625b8f83f8b3bf276ce
SanazRezvani <124499661+SanazRezvani@users.noreply.github.com>
2024-12-17 15:31:39 +1100

    Update data_dlxtr_automate.py

--- data_dlxtr_automate.py
+++ data_dlxtr_automate.py
@@ -3,7 +3,7 @@ import zipfile
 import pandas as pd
 
 # Define dataset information
-dataset = "andrewmvd/heart-failure-clinical-data"  # Replace with your dataset identifier
+dataset = ".../...."  # Replace with your dataset identifier
 download_dir = "./data"  # Local directory to save the dataset
 
 # Ensure the directory exists
@@ -13,12 +13,12 @@ os.makedirs(download_dir, exist_ok=True)
 os.system(f"kaggle datasets download -d {dataset} -p {download_dir}")
 
 # Unzip the dataset
-zip_path = os.path.join(download_dir, "heart-failure-clinical-data.zip")  # Replace with your zip file name
+zip_path = os.path.join(download_dir, ".....zip")  # Replace with your zip file name
 with zipfile.ZipFile(zip_path, "r") as zip_ref:
     zip_ref.extractall(download_dir)
 
 # Load the CSV file into a DataFrame
-csv_path = os.path.join(download_dir, "heart_failure_clinical_records_dataset.csv")  # Replace with your CSV file name
+csv_path = os.path.join(download_dir, ".....csv")  # Replace with your CSV file name
 df = pd.read_csv(csv_path)
 
 # Display the DataFrame
